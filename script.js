959d496fdf5d006df706bd66da07a3e1afaeb334
sushant kumar <ksushant0293@gmail.com>
2024-03-08 16:18:39 +0530

    made basic UI, added questions and answers in the scrit

---
+++ script.js
@@ -0,0 +1,46 @@
+const questions = [
+    {   question: "How many rings on the Olympic flag", 
+        answer: [
+            {text: five , correct: true},
+            {text: seven, correct: false },
+            {text:four, correct: false},
+            {text:six, correct: false}
+        ]
+    },
+    {
+        question: "What is the longest river in the world?",
+        answer: [
+            {text: Amazon river , correct: false},
+            {text: The Nile river, correct: true },
+            {text: Yellow river, correct: false},
+            {text: Congo river, correct: false}
+        ]
+    },
+    {
+        question: "Which desert is the largest in the world?",
+        answer: [
+            {text: Thar desert , correct: false},
+            {text: Gobi desert, correct: false },
+            {text: Sahara desert, correct: true},
+            {text: Karakum desert, correct: false}
+        ]
+    },
+    {
+        question: "What is the smallest country in the world?",
+        answer: [
+            {text: Malta , correct: false},
+            {text: Nauru, correct: false },
+            {text:Monaco, correct: false},
+            {text:Vatican city, correct: true}
+        ]
+    },
+    {
+        question: "Who is known as the fastest man in the world?",
+        answer: [
+            {text:Usain Bolt, correct: true},
+            {text: sushant kumar , correct: false},
+            {text: rahul gandhi, correct: false },
+            {text:narendra modi, correct: false}
+        ]
+    }
+]
